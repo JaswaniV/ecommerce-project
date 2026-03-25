c94362b9e56088f0b93cb216cabe9d54e875d573
JaswaniV <vjaswan78@gmail.com>
2026-03-25 16:44:03 +0530

    Seperate into smaller components

---
+++ src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,51 @@
+import dayjs from "dayjs";
+import DeliveryOptions from "./DeliveryOptions";
+
+function OrderSummary( { cart, deliveryOptions } ) {
+  return (
+    <div className="order-summary">
+      {cart.map((item) => {
+        const selectedDeliveryOption = deliveryOptions.find(
+          (option) => option.id === item.deliveryOptionId,
+        );
+
+        return (
+          <div key={item.productId} className="cart-item-container">
+            <div className="delivery-date">
+              Delivery date:{" "}
+              {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
+                "dddd, MMMM D",
+              )}
+            </div>
+
+            <div className="cart-item-details-grid">
+              <img className="product-image" src={item.product.image} />
+
+              <div className="cart-item-details">
+                <div className="product-name">{item.product.name}</div>
+                <div className="product-price">
+                  Rs.{item.product.priceCents}
+                </div>
+                <div className="product-quantity">
+                  <span>
+                    Quantity:{" "}
+                    <span className="quantity-label">{item.quantity}</span>
+                  </span>
+                  <span className="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span className="delete-quantity-link link-primary">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <DeliveryOptions deliveryOptions={deliveryOptions} item={item} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default OrderSummary;
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Header from "../../components/Header"; 
+import ProductsGrid from "./ProductsGrid";
+import "./HomePage.css";
+
+function HomePage( { cart } ) { //Destructured the cart prop that is being passed from App.jsx
+  const[products, setProducts] = useState([]); //Making initial values as empty 
+
+  
+  useEffect( () => {
+    axios.get('api/products')
+      .then( (response) => { 
+        setProducts(response.data);
+      })  
+
+  }, [] ); //Passed and empty dependency array to make sure that the API call is made only once when the component is mounted
+
+
+
+  return (
+    <>
+      <title>Home</title>
+
+      < Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
