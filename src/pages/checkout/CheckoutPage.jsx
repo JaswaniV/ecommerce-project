d77c4fae07059690b70b3f3a40ea08f9c622c50a
JaswaniV <vjaswan78@gmail.com>
2026-03-26 14:19:18 +0530

    Use Checkout Header as a component

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import OrderSummary from "./OrderSummary";
 import PaymentSummary from "./PaymentSummary"; 
+import CheckoutHeader from "./CheckoutHeader";
 import "./checkout-header.css";
 import "./CheckoutPage.css";
+
 
 function CheckoutPage({ cart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
@@ -27,28 +29,7 @@ function CheckoutPage({ cart }) {
     <>
       <title>Checkout</title>
 
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <a className="return-to-home-link" href="/">
-              3 items
-            </a>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeader />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
