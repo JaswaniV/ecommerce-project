30e3557bd2d6c3d0d5840cb164efd62d73684793
JaswaniV <vjaswan78@gmail.com>
2026-04-03 02:54:14 +0530

    Make checkout header item count dynamic

--- ecommerce-project-ts/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project-ts/src/pages/checkout/CheckoutHeader.jsx
@@ -1,40 +1,50 @@
 import { Link } from "react-router";
-import { IMAGE_URL } from "../../apii"; //  
+import { IMAGE_URL } from "../../apii"; 
 
-function CheckoutHeader(){
-    return (
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/"> 
-              <h2 style={{
-                fontSize: "20px",
-                fontWeight: "700",
-                color: "black",
-                textDecoration: "none"
-              }}>
-                VJ Commerce
-              </h2>
-            </Link>
-          </div>
 
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <Link className="return-to-home-link" to="/">
-              3 items
-            </Link>
-            )
-          </div>
+function CheckoutHeader({ cart }) {
 
-          <div className="checkout-header-right-section">
-            <img 
-              src={`${IMAGE_URL}/images/icons/checkout-lock-icon.png`}
-              
-            />
-          </div>
+  
+  let totalItems = 0;
+  cart.forEach((item) => {
+    totalItems += item.quantity;
+  });
+
+  return (
+    <div className="checkout-header">
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/"> 
+            <h2 style={{
+              fontSize: "20px",
+              fontWeight: "700",
+              color: "black",
+              textDecoration: "none"
+            }}>
+              VJ Commerce
+            </h2>
+          </Link>
+        </div>
+
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            
+            
+            {totalItems} items
+            
+          </Link>
+          )
+        </div>
+
+        <div className="checkout-header-right-section">
+          <img 
+            src={`${IMAGE_URL}/images/icons/checkout-lock-icon.png`}
+          />
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default CheckoutHeader;
